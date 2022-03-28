50f0f2066ca08453e63cd4d5ebbdef35242f1f7c
Ilya Azin <martis.azin@gmail.com>
2022-03-28 23:53:21 +0300

    TASK-5: Setup IndexPage (styles with search-bar)

--- website/infosearch/views.py
+++ website/infosearch/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {}
-    return render(request, 'infosearch/index.html', context)
+    return render(request, 'infosearch/index/index.html', context)
